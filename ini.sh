f4356fcc8fd8e6a5f05a75b0b1a228b682a3ee91
chaozk <ccaozk@gmail.com>
2017-01-02 03:35:33 +0000

    upd

---
+++ ini.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo 'some dependentes and files'
+sudo apt-get install bluez pulseaudio-module-bluetooth python-gobject python-gobject-2 cmus mplayer vim
+
+echo 'pihole'
+
+curl -L https://install.pi-hole.net | bash
+
+echo 'customization'
+
+mkdir Paketler/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cd Paketler
+git clone https://github.com/ozkancc/dotfiles 
+git clone https://github.com/np1/mps
+cp dotfiles/.vimrc ~/.vimrc
+
+echo 'dont forget to change the passwd'
+
+
